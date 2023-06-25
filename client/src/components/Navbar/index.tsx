c8d69d4c611dc1ab1a679e3ae4f895e9edd0725d
sivansuh <sivansuh.is@gmail.com>
2023-06-25 12:59:02 +0300

    redux-persist added

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Style from "./style.module.css";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 const Navbar = () => {
+  const { isLoggin } = useSelector((state: RootState) => state.user);
   return (
     <div className={Style.navbar}>
       <div className={Style.flexStructure}>
@@ -11,12 +14,20 @@ const Navbar = () => {
         </Link>
         <div>Search</div>
         <div className="flex items-center gap-2">
-          <Link href="/auth/login">
-            <div className={Style.button}>Login</div>
-          </Link>
-          <Link href="/auth/register">
-            <div className={Style.register}>Register</div>
-          </Link>
+          {isLoggin ? (
+            <Link href="/auth/login">
+              <div className={Style.button}>Logout</div>
+            </Link>
+          ) : (
+            <>
+              <Link href="/auth/login">
+                <div className={Style.button}>Login</div>
+              </Link>
+              <Link href="/auth/register">
+                <div className={Style.register}>Register</div>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,12 +1,23 @@
-import {configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore } from '@reduxjs/toolkit'
 import authSlice from './auth/authSlice'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from "redux-persist/lib/storage"
 import { useDispatch } from 'react-redux'
 
-export const store = configureStore({
-    reducer:{
-        user:authSlice
-    }
+const rootReducer = combineReducers({
+    user:authSlice
 })
 
+const persistConfig = {
+    key:"root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
+export const store = configureStore({
+    reducer:persistedReducer
+})
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export const AppDispatch : () => typeof store.dispatch = useDispatch;
